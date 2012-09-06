7b4e254c73a9fd9c534e54579bf948e12914a643
Pedro Henrique <phinfonet@Phinfonet-MacBook-Pro.local>
2012-09-06 20:39:37 -0300

    Initial Commit

---
+++ PCalc/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  PCalc
+#
+#  Created by Pedro Henrique on 9/6/12.
+#  Copyright 2012 Pedro Henrique. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
